3cbaaf4a9ae0a11e8d40229f35378709ca89f576
Emre Cimen <ecimen@anadolu.edu.tr>
2017-09-11 00:07:38 +0300

    Update ICF_Purity.py

--- ICF_Purity.py
+++ ICF_Purity.py
@@ -1,18 +1,31 @@
-
+# This file is the implementation of the algorithm 2
 
 from sklearn.cluster import KMeans
 import numpy as np
 
 ####################### PARAMETERS #############################
-# Set Algorithm 2 parameters here
+# Set Algorithm 2 Inputs
+#The parameter defines the ratio of how much of redundant data are to be eliminated from set A.
+# As tau1 increase, more data elimination occurs.
+# Similarly,tau2 defines the data elimination ratio for set B.
+# If more data is eliminated from sets, the operations become fast while reducing the training accuracy.
+# The other two parameters,tau3 and tau4, define thresholds to increase/decrease number of sub-sets.
+# tau1 and tau2 is from [0, inf]; tau3 and tau4 is from [0, 1]
 tau1=0.9
 tau2=1.1
-tau3=0.90
+tau3=0.95
 tau4=0.05
 
 ################################################################
 
-#implementation of Algorithm 2 Step 3
+#This function is the implementation of Algorithm 2 Step 3
+#Inputs:
+# set A: the class to be classified
+# set B: rest of the all classes
+# c: center of the cluster
+# r: radius for set A
+# rb: radius for set B
+# Output: Eliminated sets; A and B
 def eliminateWithR(A, B, c, r, rb):
 
     bordA=r*tau1
@@ -47,6 +60,9 @@ def eliminateWithR(A, B, c, r, rb):
 
 
 #Starting of Algorithm 2. Step 1 implementation
+#Inputs:
+# set A: the class to be classified
+# set B: rest of the all classes
 def getPureClusters(A, B):
 
     stop=0
@@ -107,7 +123,11 @@ def getPureClusters(A, B):
 
     return centroids, DpntClusters, ResR, ResRB, pr
 
-# implementation of Algorithm 2 Step 2
+# This function is the implementation of Algorithm 2 Step 2
+# set A: the class to be classified
+# set B: rest of the all classes
+# c: center of the cluster
+# Output: puri: purity rate of the cluster and set A-B radiuses
 def purity (a, b, c):
 
 
@@ -125,4 +145,7 @@ def purity (a, b, c):
 
 
     return puri, rada, radb
+
+
+
 
